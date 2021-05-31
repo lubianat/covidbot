f2d8b75f211fcaa642e8588d8a22b997feb48e63
lubianat <tiago.lubiana.alves@usp.br>
2021-05-31 10:38:09 -0300

    code: Fix problem with pandas version

--- wikipedia_case_import/pywiki_import.py
+++ wikipedia_case_import/pywiki_import.py
@@ -1,8 +1,8 @@
-from bs4 import BeautifulSoup # library to parse HTML documents
+from bs4 import BeautifulSoup  # library to parse HTML documents
 from datetime import datetime
 from getpass import getpass
-import pandas as pd # library for data analysis
-import requests # library to handle requests
+import pandas as pd  # library for data analysis
+import requests  # library to handle requests
 import os
 import pywikibot
 from datetime import datetime
@@ -11,44 +11,56 @@ from datetime import datetime
 def prepare_to_wikidata(cases_df, name="cases"):
     last_column = cases_df.columns[-1]
     # Get only country-level data
-    country_level_df = cases_df[["Country/Region", last_column]][cases_df["Province/State"].isna()]
+    country_level_df = cases_df[["Country/Region", last_column]][
+        cases_df["Province/State"].isna()
+    ]
 
     # Check which countries have only an state-level measure and aggregate
-    state_level = set(cases_df["Country/Region"]) - set(country_level_df["Country/Region"])
-    cases_df_to_aggregate = cases_df[["Province/State", "Country/Region", last_column]][[a in state_level for a in cases_df["Country/Region"]]]
+    state_level = set(cases_df["Country/Region"]) - set(
+        country_level_df["Country/Region"]
+    )
+    cases_df_to_aggregate = cases_df[["Province/State", "Country/Region", last_column]][
+        [a in state_level for a in cases_df["Country/Region"]]
+    ]
 
-    cases_df_to_aggregate = cases_df_to_aggregate.groupby(['Country/Region']).sum()
+    cases_df_to_aggregate = cases_df_to_aggregate.groupby(["Country/Region"]).sum()
     cases_df_to_aggregate["Country/Region"] = cases_df_to_aggregate.index
-    country_level_df = country_level_df.append(cases_df_to_aggregate, ignore_index=True)
+    country_level_df = country_level_df.append(
+        cases_df_to_aggregate, ignore_index=True, sort=False
+    )
 
     # Change names for readability
     country_level_df.columns = ["country", name]
-
 
     # Fix names to match Wikidata and add outbreak identifiers
 
     local_outbreak_items = pd.read_csv("reference.csv")
 
     template_to_label = {
-        'Bahamas': 'The Bahamas',
-        'Burma':"Myanmar",
+        "Bahamas": "The Bahamas",
+        "Burma": "Myanmar",
         "Cabo Verde": "Cape Verde",
-        'China': 'mainland China',
-        'Congo (Brazzaville)': 'Republic of the Congo',
-        'Congo (Kinshasa)': 'Democratic Republic of the Congo',
-        "Cote d'Ivoire":"Ivory Coast",
-        "Czechia":"Czech Republic",
-        'Gambia': 'The Gambia',
+        "China": "mainland China",
+        "Congo (Brazzaville)": "Republic of the Congo",
+        "Congo (Kinshasa)": "Democratic Republic of the Congo",
+        "Cote d'Ivoire": "Ivory Coast",
+        "Czechia": "Czech Republic",
+        "Gambia": "The Gambia",
         "Korea, South": "South Korea",
         "Sao Tome and Principe": "São Tomé and Príncipe",
-        'US': 'United States of America',
-        "Taiwan*":"Taiwan",
-        "Timor-Leste":"East Timor" 
+        "US": "United States of America",
+        "Taiwan*": "Taiwan",
+        "Timor-Leste": "East Timor",
     }
 
-    country_level_df["country"] = country_level_df["country"].replace(template_to_label)
+    print(country_level_df["country"])
+    country_level_df["country"] = country_level_df["country"].replace(
+        template_to_label, regex=False
+    )
 
-    reconciled = country_level_df.merge(local_outbreak_items, left_on="country", right_on="countryLabel").drop_duplicates()
+    reconciled = country_level_df.merge(
+        local_outbreak_items, left_on="country", right_on="countryLabel"
+    ).drop_duplicates()
 
     # Check which entries were not reconciled
     print("Not reconciled:")
@@ -56,15 +68,20 @@ def prepare_to_wikidata(cases_df, name="cases"):
 
     return reconciled
 
+
 print("====== Pulling cases ======")
-cases_df = pd.read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+cases_df = pd.read_csv(
+    "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+)
 
 last_column = cases_df.columns[-1]
 
 reconciled_cases = prepare_to_wikidata(cases_df, name="cases")
 
 print("====== Pulling deaths ======")
-deaths_df = pd.read_csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+deaths_df = pd.read_csv(
+    "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+)
 
 reconciled_deaths = prepare_to_wikidata(deaths_df, name="deaths")
 
@@ -149,104 +166,127 @@ for i, row in reconciled.iterrows():
             continue
 
         item = pywikibot.ItemPage(repo, row["item"])
-        item.get() #Fetch all page data, and cache it.
+        item.get()  # Fetch all page data, and cache it.
 
         # Change rank of current deaths to normal
         # (code based on https://gist.github.com/urbanecm/f0b2bb95514f322207d04908cc4d8143)
         itemData = item.get()
-        death_claims = itemData['claims'].get('P1120', [])
+        death_claims = itemData["claims"].get("P1120", [])
         for claim in death_claims:
-                if claim.rank != "preferred":
-                        continue
-                claim.setRank('normal')
-                item.editEntity({
-                        "claims": [ claim.toJSON() ]
-                }, summary='Change P1120 rank to normal')
+            if claim.rank != "preferred":
+                continue
+            claim.setRank("normal")
+            item.editEntity(
+                {"claims": [claim.toJSON()]}, summary="Change P1120 rank to normal"
+            )
 
         # Add number of deaths
 
-
         ## Value --> death count
 
-        deaths_claim = pywikibot.Claim(repo, u'P1120') # Adding number of deaths (P1120)
-        deaths_claim.setTarget(pywikibot.WbQuantity(row["deaths"])) #Set the target value in the local object.
+        deaths_claim = pywikibot.Claim(
+            repo, u"P1120"
+        )  # Adding number of deaths (P1120)
+        deaths_claim.setTarget(
+            pywikibot.WbQuantity(row["deaths"])
+        )  # Set the target value in the local object.
         deaths_claim.setRank("preferred")
-        item.addClaim(deaths_claim, summary=u'Adding death count from template')
+        item.addClaim(deaths_claim, summary=u"Adding death count from template")
 
-        ## Qualifier --> date 
+        ## Qualifier --> date
 
-        qualifier = pywikibot.Claim(repo, u'P585') # Adding qualifier of point in time (P585)
-        target = pywikibot.WbTime(year=int(date_of_data.strftime("%Y")), month=int(date_of_data.strftime("%m")), day=int(date_of_data.strftime("%d")))
+        qualifier = pywikibot.Claim(
+            repo, u"P585"
+        )  # Adding qualifier of point in time (P585)
+        target = pywikibot.WbTime(
+            year=int(date_of_data.strftime("%Y")),
+            month=int(date_of_data.strftime("%m")),
+            day=int(date_of_data.strftime("%d")),
+        )
         qualifier.setTarget(target)
-        deaths_claim.addQualifier(qualifier, summary=u'Adding date.')
-
-
+        deaths_claim.addQualifier(qualifier, summary=u"Adding date.")
 
         ## Source --> Wikipedia template URL
-        wiki_url = pywikibot.Claim(repo, u'P854') # reference URL (P854)
+        wiki_url = pywikibot.Claim(repo, u"P854")  # reference URL (P854)
         wiki_url.setTarget("https://github.com/CSSEGISandData/COVID-19")
-        
+
         ## Source --> Reference of the article that describes the data
         # This is a request on the GitHub repository of the datasource
-        ref = pywikibot.Claim(repo, u'P248') #stated in (P248)
-        ref.setTarget(pywikibot.ItemPage(repo, 'Q87456354'))
+        ref = pywikibot.Claim(repo, u"P248")  # stated in (P248)
+        ref.setTarget(pywikibot.ItemPage(repo, "Q87456354"))
 
         ## Source --> Retrieved today
-        today = datetime.today() #Date today
-        retrieved = pywikibot.Claim(repo, u'P813') #retrieved (P813). Data type: Point in time
-        retrieved_target = pywikibot.WbTime(year=int(today.strftime("%Y")), month=int(today.strftime("%m")), day=int(today.strftime("%d"))) #retrieved -> %DATE TODAY%. Example retrieved -> 29.11.2020
-        retrieved.setTarget(retrieved_target) #Inserting value
+        today = datetime.today()  # Date today
+        retrieved = pywikibot.Claim(
+            repo, u"P813"
+        )  # retrieved (P813). Data type: Point in time
+        retrieved_target = pywikibot.WbTime(
+            year=int(today.strftime("%Y")),
+            month=int(today.strftime("%m")),
+            day=int(today.strftime("%d")),
+        )  # retrieved -> %DATE TODAY%. Example retrieved -> 29.11.2020
+        retrieved.setTarget(retrieved_target)  # Inserting value
 
-        deaths_claim.addSources([ref,wiki_url, retrieved], summary=u'Adding sources.')
-
+        deaths_claim.addSources([ref, wiki_url, retrieved], summary=u"Adding sources.")
 
         # Change rank of current deaths to normal
         # (code based on https://gist.github.com/urbanecm/f0b2bb95514f322207d04908cc4d8143)
-        cases_claim = itemData['claims'].get('P1603', [])
+        cases_claim = itemData["claims"].get("P1603", [])
         for claim in cases_claim:
-                if claim.rank != "preferred":
-                        continue
-                claim.setRank('normal')
-                item.editEntity({
-                        "claims": [ claim.toJSON() ]
-                }, summary='Change P1603 rank to normal')
+            if claim.rank != "preferred":
+                continue
+            claim.setRank("normal")
+            item.editEntity(
+                {"claims": [claim.toJSON()]}, summary="Change P1603 rank to normal"
+            )
 
-        
         # Add number of cases
 
         ## Value --> case count
 
-        cases_claim = pywikibot.Claim(repo, u'P1603') # Adding number of cases (P1603)
-        cases_claim.setTarget(pywikibot.WbQuantity(row["cases"])) #Set the target value in the local object.
+        cases_claim = pywikibot.Claim(repo, u"P1603")  # Adding number of cases (P1603)
+        cases_claim.setTarget(
+            pywikibot.WbQuantity(row["cases"])
+        )  # Set the target value in the local object.
         cases_claim.setRank("preferred")
-        item.addClaim(cases_claim, summary=u'Adding case count from template')
+        item.addClaim(cases_claim, summary=u"Adding case count from template")
 
+        ## Qualifier --> date
 
-        ## Qualifier --> date 
-
-        qualifier = pywikibot.Claim(repo, u'P585') # Adding qualifier of point in time (P585)
-        target = pywikibot.WbTime(year=int(date_of_data.strftime("%Y")), month=int(date_of_data.strftime("%m")), day=int(date_of_data.strftime("%d")))
+        qualifier = pywikibot.Claim(
+            repo, u"P585"
+        )  # Adding qualifier of point in time (P585)
+        target = pywikibot.WbTime(
+            year=int(date_of_data.strftime("%Y")),
+            month=int(date_of_data.strftime("%m")),
+            day=int(date_of_data.strftime("%d")),
+        )
         qualifier.setTarget(target)
-        cases_claim.addQualifier(qualifier, summary=u'Adding date.')
-
+        cases_claim.addQualifier(qualifier, summary=u"Adding date.")
 
         ## Source --> Wikipedia template URL
-        wiki_url = pywikibot.Claim(repo, u'P854') # reference URL (P854)
+        wiki_url = pywikibot.Claim(repo, u"P854")  # reference URL (P854)
         wiki_url.setTarget("https://github.com/CSSEGISandData/COVID-19")
-        
+
         ## Source --> Reference of the article that describes the data
         # This is a request on the GitHub repository of the datasource
-        ref = pywikibot.Claim(repo, u'P248') #stated in (P248)
-        ref.setTarget(pywikibot.ItemPage(repo, 'Q87456354'))
+        ref = pywikibot.Claim(repo, u"P248")  # stated in (P248)
+        ref.setTarget(pywikibot.ItemPage(repo, "Q87456354"))
 
         ## Source --> Retrieved today
-        today = datetime.today() #Date today
-        retrieved = pywikibot.Claim(repo, u'P813') #retrieved (P813). Data type: Point in time
-        retrieved_target = pywikibot.WbTime(year=int(today.strftime("%Y")), month=int(today.strftime("%m")), day=int(today.strftime("%d"))) #retrieved -> %DATE TODAY%. Example retrieved -> 29.11.2020
-        retrieved.setTarget(retrieved_target) #Inserting value
+        today = datetime.today()  # Date today
+        retrieved = pywikibot.Claim(
+            repo, u"P813"
+        )  # retrieved (P813). Data type: Point in time
+        retrieved_target = pywikibot.WbTime(
+            year=int(today.strftime("%Y")),
+            month=int(today.strftime("%m")),
+            day=int(today.strftime("%d")),
+        )  # retrieved -> %DATE TODAY%. Example retrieved -> 29.11.2020
+        retrieved.setTarget(retrieved_target)  # Inserting value
 
-        cases_claim.addSources([ref,wiki_url, retrieved], summary=u'Adding sources.')
-    
-    except Exception as e: print(e)
+        cases_claim.addSources([ref, wiki_url, retrieved], summary=u"Adding sources.")
 
-    
+    except Exception as e:
+        print(e)
+
